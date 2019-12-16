1dae865fe27424d31f373237c8a11845c4150534
sachinm78 <sachinm78@gmail.com>
2019-12-16 14:36:24 -0600

    made a copy of useState lessons; will edit to practice useEffect hook

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react"
+
+function App() {
+    const [inputData, setInputData] = useState({firstName: "", lastName: ""})
+    const [contactsData, setContactsData] = useState([])
+    
+    function handleChange(event) {
+        // update our inputData state
+        const {name, value} = event.target
+        setInputData(prevInputData => ({...prevInputData, [name]: value}))
+    }
+    
+    function handleSubmit(event) {
+        // add the inputData to the contactsData array
+        event.preventDefault()
+        setContactsData(prevContacts => [...prevContacts, inputData])
+    }
+    
+    const contacts = contactsData.map(contact => <h2 key={contact.firstName + contact.lastName}>{contact.firstName} {contact.lastName}</h2>)
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    placeholder="First Name"
+                    name="firstName" 
+                    value={inputData.firstName}
+                    onChange={handleChange}
+                />
+                <input 
+                    placeholder="Last Name"
+                    name="lastName" 
+                    value={inputData.lastName}
+                    onChange={handleChange}
+                />
+                <br />
+                <button>Add contact</button>
+            </form>
+            {contacts}
+        </>
+    )
+}
+
+export default App
