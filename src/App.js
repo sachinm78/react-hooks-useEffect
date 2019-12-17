dc85c46a80b45cd5dcc1a1ad5e98fce90900dc4e
sachinm78 <sachinm78@gmail.com>
2019-12-17 16:07:56 -0600

    successfully implemented useEffect hook to randomly change color when counter changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
+import randomcolor from "randomcolor"
 
 function App() {
     const [count, setCount] = useState(0)
-    const [answer, setAnswer] = useState("Yes")
+    const [color, setColor] = useState("")
     
     function increment() {
         setCount(prevCount => prevCount + 1)
@@ -12,9 +13,13 @@ function App() {
         setCount(prevCount => prevCount - 1)
     }
     
+    useEffect(() => {
+        setColor(randomcolor())
+    }, [count])
+    
     return (
         <div>
-            <h1>{count}</h1>
+            <h1 style={{color: color}}>{count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
